e8c37b3cf2e8c7ab82b90524e67fd5c94be7fde1
Samuel Souza <samuelsouza.contact@gmail.com>
2019-02-16 09:19:25 -0300

    course entity creation test

--- backend/src/test/java/br/samuelpsouza/matrizcurricular/MatrizCurricularApplicationTests.java
+++ backend/src/test/java/br/samuelpsouza/matrizcurricular/MatrizCurricularApplicationTests.java
@@ -1,5 +1,6 @@
 package br.samuelpsouza.matrizcurricular;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,5 +14,10 @@ public class MatrizCurricularApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+    public void shouldCreateACourseObject(){
+	    Course course = new Course("Ciencia da Computação");
+        Assert.assertTrue(course != null);
+    }
 }
 
