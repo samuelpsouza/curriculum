b4b1d50af496f63260b82609b97c37eae150b170
Samuel Souza <samuelsouza.contact@gmail.com>
2019-02-16 09:47:36 -0300

    course entity persistence test new attributes

--- backend/src/test/java/br/samuelpsouza/matrizcurricular/MatrizCurricularApplicationTests.java
+++ backend/src/test/java/br/samuelpsouza/matrizcurricular/MatrizCurricularApplicationTests.java
@@ -3,26 +3,36 @@ package br.samuelpsouza.matrizcurricular;
 import br.samuelpsouza.matrizcurricular.model.Course;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MatrizCurricularApplicationTests {
+    @Autowired
+    private CourseRepository courseRepository;
+    private Course course;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
-	@Test
-    public void shouldCreateACourseObject(){
-	    Course course = new Course("Ciencia da Computação");
+    @Test
+    public void shouldCreateACourseObject() {
+        course = new Course("Ciencia da Computação");
         assertNotNull(course);
         assertNotNull(course.getTitle());
     }
 
-
+    @Test
+    public void shouldCreateAndPersistAObject() {
+        course = new Course("Ciencia da Computação");
+        Course persistedCourse = this.courseRepository.save(course);
+        assertTrue(persistedCourse.getCode().equals(course.getCode()));
+    }
 }
 
