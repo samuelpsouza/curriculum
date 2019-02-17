24a03af6a58255f1c58e83aa8440631e8c14eb8e
Samuel Souza <samuelsouza.contact@gmail.com>
2019-02-17 20:18:17 -0300

    fetching majors from backend server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 import './App.css';
 
 import Major from './components/major/Major';
 import AppBar from './components/app-bar/AppBar';
+
+const styles = theme => ({
+  fab: {
+    margin: theme.spacing.unit,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
+});
+
 
 class App extends Component {
   state = {
@@ -14,15 +27,22 @@ class App extends Component {
       headers: {
         "Content-Type": "application/json"
       }
-    }).then(response => {
-      console.log(response)
+    })
+    .then(response => response.json())
+    .then(response => {
+      this.setState({...this.state, majors: response.data.content})
     })
   }
 
   render() {
+    const { classes } = this.props;
     return (
       <div className="App">
         <AppBar />
+
+        <Fab color="primary" aria-label="Add" className={classes.fab}>
+          <AddIcon />
+        </Fab>
 
         {
           this.state.majors.map(major => {
@@ -36,4 +56,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
