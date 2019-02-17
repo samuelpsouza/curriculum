7d8f329b887a566e2a52eebfb5b35b334a8ad827
Samuel Souza <samuelsouza.contact@gmail.com>
2019-02-17 17:46:28 -0300

    basic frontend layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Major from './components/major/Major';
+import AppBar from './components/app-bar/AppBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppBar />
 
+        <Major />
       </div>
     );
   }
--- frontend/src/components/major/Major.jsx
+++ frontend/src/components/major/Major.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-export default props = props => {
-    return (<p>Major</p>)
+const styles = theme => ({
+    root: {
+      ...theme.mixins.gutters(),
+      paddingTop: theme.spacing.unit * 2,
+      paddingBottom: theme.spacing.unit * 2,
+    },
+});
+
+
+const Major = props => {
+    const { classes } = props;
+    return (
+        <Paper className={classes.root} elevation={1}>
+          <Typography variant="h5" component="h3">
+            This is a sheet of paper.
+          </Typography>
+          <Typography component="p">
+            Paper can be used to build surface or other elements for your application.
+          </Typography>
+        </Paper>
+    )
 }
+
+export default withStyles(styles)(Major);
