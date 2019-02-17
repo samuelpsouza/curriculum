7529c3ad6c754d7053ccf862c4a7f9e3eaa38b7b
Samuel Souza <samuelsouza.contact@gmail.com>
2019-02-17 18:54:38 -0300

    integrating backend and frontend

--- backend/src/main/java/br/samuelpsouza/matrizcurricular/controller/MajorController.java
+++ backend/src/main/java/br/samuelpsouza/matrizcurricular/controller/MajorController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "majors", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
 public class MajorController {
     private final MajorService majorService;
