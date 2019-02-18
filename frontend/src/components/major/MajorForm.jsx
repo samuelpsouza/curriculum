2b22c44ee2eb4b9bab86ff64db8368b019e9cc36
Samuel Souza <samuelsouza.contact@gmail.com>
2019-02-17 22:08:36 -0300

    add major form

--- frontend/src/components/major/MajorForm.jsx
+++ frontend/src/components/major/MajorForm.jsx
@@ -5,8 +5,27 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import MenuItem from '@material-ui/core/MenuItem';
+
+
+const periods = [
+    {
+      value: 'MANHA',
+      label: 'Manhã',
+    },
+    {
+      value: 'TARDE',
+      label: 'Tarde',
+    },
+    {
+      value: 'NOITE',
+      label: 'Noite',
+    }
+  ];
 
 export default props => {
+    let period = '';
+    
     return (
         <Dialog
           open={props.open}
@@ -15,14 +34,18 @@ export default props => {
         >
           <DialogTitle id="form-dialog-title">Novo Curso</DialogTitle>
           <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="title"
-              label="Título"
-              type="text"
-              fullWidth
-            />
+            <TextField autoFocus margin="dense" id="code" label="Código" type="text" fullWidth/>
+            <TextField margin="dense" id="title" label="Título" type="text" fullWidth/>
+            <TextField margin="dense" id="description" label="Descrição" type="textarea" fullWidth/>
+            <TextField margin="dense" id="period" select label="Período" value={period} fullWidth>
+                {periods.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+            <TextField margin="dense" id="duration" label="Duração" type="text" fullWidth/>
+            <TextField margin="dense" id="registrationNumber" label="Vagas Disponíveis" type="number" fullWidth/>
           </DialogContent>
           <DialogActions>
             <Button onClick={props.handleClose} color="primary">
