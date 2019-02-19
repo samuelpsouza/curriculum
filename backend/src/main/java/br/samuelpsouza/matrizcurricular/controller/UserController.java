cfa2ee4e12ade9b7b85870955124d770afa19281
Samuel Souza <samuelsouza.contact@gmail.com>
2019-02-19 18:45:18 -0300

    creating user controller

--- backend/src/main/java/br/samuelpsouza/matrizcurricular/controller/UserController.java
+++ backend/src/main/java/br/samuelpsouza/matrizcurricular/controller/UserController.java
@@ -1,12 +1,40 @@
 package br.samuelpsouza.matrizcurricular.controller;
 
+import br.samuelpsouza.matrizcurricular.model.User;
+import br.samuelpsouza.matrizcurricular.payload.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("users")
 @PreAuthorize("hasRole('ADMIN')")
 public class UserController {
+    private final UserService userService;
 
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<ApiResponse> getUsers() {
+        return ResponseEntity.ok(this.userService.getUsers());
+    }
+
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity<ApiResponse> addUser(@RequestBody @Valid User user) {
+        return ResponseEntity.ok(this.userService.saveUser(user));
+    }
+
+    @PutMapping
+    @ResponseBody
+    public ResponseEntity<ApiResponse> updateUser(@RequestBody @Valid User user) {
+        return ResponseEntity.ok(this.userService.saveUser(user));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(this.userService.deleteUser(id));
+    }
 }
