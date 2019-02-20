114ba72cc5e2c37dcddfa8bbc564ad2e403c6b31
Samuel Souza <samuelsouza.contact@gmail.com>
2019-02-20 14:12:59 -0300

    login form with redux

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -4,9 +4,14 @@ import MajorList from './components/major/MajorList';
 import CourseList from './components/course/CourseList';
 import Users from './components/user/Users';
 import Login from './components/user/Login';
-import PrivateRoute from './PrivateRoute';
 
-const AppRoutes = ({ history, user }) => (
+const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
+    <Route {...rest} render={(props) => (
+      isAuthenticated ? (<Component {...props} />) : (<Redirect to='/login' />)
+    )} />
+)
+
+const AppRoutes = ({ history, isAuthenticated }) => (
     <Router history={history}>
         <Switch>
             <Route path='/' exact component={Login} />
@@ -15,8 +20,8 @@ const AppRoutes = ({ history, user }) => (
 
             {/* Private Routes */}
 
-            <PrivateRoute exact path='/courses' isAuthenticated={user.isAuthenticated} component={CourseList}/>
-            <PrivateRoute exact path='/users' isAuthenticated={user.isAuthenticated} component={Users} />
+            <PrivateRoute exact path='/courses' isAuthenticated={isAuthenticated} component={CourseList}/>
+            <PrivateRoute exact path='/users' isAuthenticated={isAuthenticated} component={Users} />
 
             <Redirect from='' to='/login' />
         </Switch>
