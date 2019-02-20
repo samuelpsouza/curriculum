70ae5591aa9524945bc4ac606ca935ec4e0a4132
Samuel Souza <samuelsouza.contact@gmail.com>
2019-02-19 22:11:35 -0300

    adding login form

--- frontend/src/components/user/Login.jsx
+++ frontend/src/components/user/Login.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Dialog, DialogTitle, DialogContent,
+    DialogActions, Button, TextField } from '@material-ui/core';
 
 export default props => {
-    return (<div>Login</div>);
+    return (
+        <Dialog
+            open={this.state.openSemesterSelection}
+            aria-labelledby="form-dialog-title"
+            >  
+            <DialogTitle id="form-dialog-title">Novo Semestre</DialogTitle>
+            <DialogContent>
+                <TextField margin="dense" id="username" label="Usuário" type="text" fullWidth/>
+                <TextField margin="dense" id="password" label="Senha" type="password" fullWidth/>
+            </DialogContent>
+            <DialogActions>
+                <Button color="primary">
+                    Cancelar
+                </Button>
+                <Button color="primary">
+                    Login
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
 }
