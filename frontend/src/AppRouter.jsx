53415802a0e0f5dd96caab5ab48a77c501887c09
Samuel Souza <samuelsouza.contact@gmail.com>
2019-02-20 13:20:01 -0300

    redux

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { HashRouter, Route, Redirect, Switch } from 'react-router-dom'
+import { Router, Route, Redirect, Switch } from 'react-router-dom'
 import MajorList from './components/major/MajorList';
 import CourseList from './components/course/CourseList';
 import Users from './components/user/Users';
 import Login from './components/user/Login';
 import PrivateRoute from './PrivateRoute';
 
-const AppRoutes = props => (
-    <HashRouter>
+const AppRoutes = ({ history, user }) => (
+    <Router history={history}>
         <Switch>
-            <PrivateRoute exact path='/courses' component={CourseList}/>
-            <PrivateRoute exact path='/users' component={Users} />
-            <Route path='/majors' exact component={MajorList} />
             <Route path='/' exact component={Login} />
             <Route path='/login' exact component={Login} />
-            <Redirect from='' to='/' />
+            <Route path='/majors' exact component={MajorList} />
+
+            {/* Private Routes */}
+
+            <PrivateRoute exact path='/courses' isAuthenticated={user.isAuthenticated} component={CourseList}/>
+            <PrivateRoute exact path='/users' isAuthenticated={user.isAuthenticated} component={Users} />
+
+            <Redirect from='' to='/login' />
         </Switch>
-    </HashRouter>   
+    </Router>   
 );
 
 export default AppRoutes;
--- frontend/src/PrivateRoute.jsx
+++ frontend/src/PrivateRoute.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={props => (
-        localStorage.getItem('user')
-            ? <Component {...props} />
-            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
+    <Route {...rest} render={(props) => (
+        isAuthenticated ? (<Component {...props} />) : (<Redirect to='/login' />)
     )} />
 )
 
