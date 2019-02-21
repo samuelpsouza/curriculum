8ce93d0f6c082001e01e3e3992ab64918e96fdd1
Samuel Souza <samuelsouza.contact@gmail.com>
2019-02-21 11:47:04 -0300

    fixing init service and login

--- backend/src/main/java/br/samuelpsouza/matrizcurricular/model/User.java
+++ backend/src/main/java/br/samuelpsouza/matrizcurricular/model/User.java
@@ -8,13 +8,14 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
 import java.util.List;
 
 @Entity
 @Data
 @Table(name = "users")
 @NoArgsConstructor
-public class User {
+public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
