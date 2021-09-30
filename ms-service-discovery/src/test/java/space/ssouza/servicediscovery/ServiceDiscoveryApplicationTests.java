20b914256b3c6ccb8a1118703337a61af61db377
Samuel Souza <samuelsouza.contact@gmail.com>
2021-09-30 12:10:49 -0300

    creating service discovery

---
+++ ms-service-discovery/src/test/java/space/ssouza/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package space.ssouza.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
