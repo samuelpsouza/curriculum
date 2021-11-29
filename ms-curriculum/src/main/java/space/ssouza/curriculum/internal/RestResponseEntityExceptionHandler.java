1a147c4f0e757bba8a724cda62b361557d1f08f8
Samuel Souza <samuelsouza.contact@gmail.com>
2021-11-29 01:13:43 -0300

    creating kafka conf

--- ms-curriculum/src/main/java/space/ssouza/curriculum/internal/RestResponseEntityExceptionHandler.java
+++ ms-curriculum/src/main/java/space/ssouza/curriculum/internal/RestResponseEntityExceptionHandler.java
@@ -1,13 +1,31 @@
 package space.ssouza.curriculum.internal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
+    @Autowired
+    public RestResponseEntityExceptionHandler(final KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        kafkaTemplate.send("LOG_HTTP", ex.getMessage());
+        return super.handleMissingPathVariable(ex, headers, status, request);
+    }
 }
--- ms-log/src/main/kotlin/space/ssouza/mslog/config/KafkaConfig.kt
+++ ms-log/src/main/kotlin/space/ssouza/mslog/config/KafkaConfig.kt
@@ -1,25 +1,8 @@
 package space.ssouza.mslog.config
-
 import org.apache.kafka.clients.producer.ProducerConfig.*
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.core.DefaultKafkaProducerFactory
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.core.*
 
 @Configuration
 class KafkaConfig
-
-@Bean
-fun producerFactory(): ProducerFactory<String, String> = DefaultKafkaProducerFactory(mapOf(
-    Pair(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"),
-    Pair(RETRIES_CONFIG, 0),
-    Pair(BUFFER_MEMORY_CONFIG, 33554432),
-    Pair(KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer"),
-    Pair(VALUE_SERIALIZER_CLASS_CONFIG, "org.springframework.kafka.support.serializer.JsonSerializer")
-))
-
-@Bean
-fun kafkaTemplate(): KafkaTemplate<String, String> {
-    return KafkaTemplate(producerFactory())
-}
